de5144a6f1a4d1a06a6943556eb17098057a0329
RaymondChoi54 <Concept_ray@hotmail.com>
2018-02-28 02:23:13 -0500

    Append articles

--- main.js
+++ main.js
@@ -63,10 +63,12 @@ $("#advanced_search").click(function() {
          $('#search-options').slideUp();
     }
 });
+
 $(window).bind("load", function() {
     $('#search-options').hide();
     fillSite('https://newsapi.org/v2/top-headlines?country=us&apiKey=0c892f7ce2ee4fd09aef39ff92f65b77');
 });
+
 function getUrl() {
     var topic = $("#searchBar").val();
     var sort = $("#sort-by").val();
@@ -75,75 +77,110 @@ function getUrl() {
     if (limit == "") {
         limit = 20;
     }
-    return "https://newsapi.org/v2/everything?language="+ lang + "&sortBy=" + sort + "&pageSize=" + limit +"&q=" + topic + "&apiKey=0c892f7ce2ee4fd09aef39ff92f65b77";
+    console.log(limit)
+    return "https://newsapi.org/v2/everything?language=" + lang + "&sortBy=" + sort + "&pageSize=" + limit + "&q=" + topic + "&apiKey=0c892f7ce2ee4fd09aef39ff92f65b77";
 }
 
-//fill page with articles
 function clear() {
     $("h3").html("");
     document.getElementsByClassName("articles")[0].innerHTML = "";
 }
+
+//fill page with articles
 function fillSite(url) {
     $.ajax({
         type:'GET',
         url:url,
         success:function(data) {
             $.each(data.articles, function(i, item) {
-                // Create article
-                var article = document.createElement("div");
-                article.classList.add("article");  
-                document.getElementsByClassName("articles")[0].appendChild(article);
-
-                // Add image
-                var contain = document.createElement("div");
-                var img = document.createElement("img");
-                img.src = item.urlToImage;
-                img.style.width = "250px";
-                img.style.height = "200px";
-                article.appendChild(contain);
-                contain.classList.add("thumbnail");
-
-                contain.appendChild(img);
-
-                $(".thumbnail img").css("width", "250px");
-                $(".thumbnail img").css("height", "200px");
-                // $(".thumbnail img").css("border-radius","2%");
-
-                // Add title
-                var title = document.createElement("p");
-                var t = document.createTextNode(item.title);
-                title.appendChild(t);
-                title.classList.add("title");   
-                article.appendChild(title);
-
-                // Add data
-                var data = document.createElement("p");
-                var author = item.author
-                if(author == null) {
-                    author = "Unknown"
-                }
-                var t = document.createTextNode("Published: " + item.publishedAt + " By: " + author);
-                data.appendChild(t);
-                data.classList.add("data");   
-                article.appendChild(data);
-
-                // Add description
-                var description = document.createElement("p");
-                t = document.createTextNode(item.description);
-                description.appendChild(t);  
-                description.classList.add("description");  
-                description.classList.add("p" + i);  
-                article.appendChild(description);
-
-                // Show description
-                $(".p" + i).hide();
-                // title.addEventListener("click", function(e) {
-                //     $(".p" + i).toggle();
-                // }, false);
-                title.onclick = function() {
-                    $(".p" + i).toggle();
-                }
+                appendArticle(i, item);
             })
         }
     });
+}
+
+//add more articles to page
+function moreArticles() {
+    var topic = $("#searchBar").val();
+    var sort = $("#sort-by").val();
+    var lang = $("#language").val();
+    var limit = $("#limit").val();
+    var num_art = document.getElementsByClassName("articles")[0].children.length;
+    if(limit == "") {
+        limit = 20;
+    }
+    var page = num_art / parseInt(limit) + 1
+    var url = "https://newsapi.org/v2/everything?language=" + lang + "&sortBy=" + sort + "&pageSize=" + limit + "&page=" + page + "&q=" + topic + "&apiKey=0c892f7ce2ee4fd09aef39ff92f65b77";
+    if(topic == "") {
+        url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=0c892f7ce2ee4fd09aef39ff92f65b77';
+    }
+    console.log(url)
+    $.ajax({
+        type:'GET',
+        url:url,
+        success:function(data) {
+            $.each(data.articles, function(i, item) {
+   
+                console.log(i)
+                appendArticle(i * page, item);
+            })
+        }
+    });
+}
+
+function appendArticle(i, item) {
+    // Create article
+    var article = document.createElement("div");
+    article.classList.add("article");  
+    document.getElementsByClassName("articles")[0].appendChild(article);
+
+    // Add image
+    var contain = document.createElement("div");
+    var img = document.createElement("img");
+    img.src = item.urlToImage;
+    img.style.width = "250px";
+    img.style.height = "200px";
+    article.appendChild(contain);
+    contain.classList.add("thumbnail");
+
+    contain.appendChild(img);
+
+    $(".thumbnail img").css("width", "250px");
+    $(".thumbnail img").css("height", "200px");
+    // $(".thumbnail img").css("border-radius","2%");
+
+    // Add title
+    var title = document.createElement("p");
+    var t = document.createTextNode(item.title);
+    title.appendChild(t);
+    title.classList.add("title");   
+    article.appendChild(title);
+
+    // Add data
+    var data = document.createElement("p");
+    var author = item.author
+    if(author == null) {
+        author = "Unknown"
+    }
+    var t = document.createTextNode("Published: " + item.publishedAt + " By: " + author);
+    data.appendChild(t);
+    data.classList.add("data");   
+    article.appendChild(data);
+
+    // Add description
+    var description = document.createElement("p");
+    t = document.createTextNode(item.description);
+    description.appendChild(t);  
+    description.classList.add("description");  
+    description.classList.add("p" + i);  
+    article.appendChild(description);
+
+    // Show description
+    $(".p" + i).hide();
+    // title.addEventListener("click", function(e) {
+    //     $(".p" + i).toggle();
+    // }, false);
+    title.onclick = function() {
+        $(".p" + i).toggle();
+    }
 }
